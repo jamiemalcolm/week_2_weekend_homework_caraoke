f70917b0d0452601faebb27c0acb3f91fc0ac578
jamiemalcolm <jmalcolm281@gmail.com>
2020-11-27 14:14:40 +0000

    1st song test passed

--- tests/song_test.py
+++ tests/song_test.py
@@ -11,3 +11,6 @@ class TestSongs(unittest.TestCase):
         self.song_2 = Songs("Arctic Monkeys", "Flourecent Adolescent", 3)
         self.song_3 = Songs("Jay-Z", "Empire State Of Mind", 5)
         self.song_4 = Songs("Chance The Rapper", "Acid Rap", 5)
+
+    def test_song_has_artist(self):
+        self.assertEqual("Kings Of Leon", self.song_1.artist)
